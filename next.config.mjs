e6911e74c158edc08bf2eacbe28073513f183568
Fabian Mareco <fabian.mareco@gmail.com>
2026-05-24 02:22:26 -0300

    Despensa San Ignacio v1.1

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,6 @@ const nextConfig = {
   images: {
     domains: ['images.pexels.com', 'www.pexels.com'],
   },
-  // Exclude @imgly from server-side bundling (browser-only library)
-  serverExternalPackages: ['@imgly/background-removal'],
-  webpack(config, { isServer }) {
-    // Enable async WebAssembly for the client bundle
-    if (!isServer) {
-      config.experiments = { ...config.experiments, asyncWebAssembly: true };
-    }
-    // Treat binary model files as static assets, not JS
-    config.module.rules.push({
-      test: /\.(onnx|wasm|bin)$/,
-      type: 'asset/resource',
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
